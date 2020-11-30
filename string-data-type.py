f6673a8f6c1e0b9ee2f8fe46544757072dff0eb8
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-30 16:17:58 -0600

    string functions

--- string-data-type.py
+++ string-data-type.py
@@ -12,3 +12,27 @@ the end of the string!''')
 
 print(len("Spaghetti"))
 
+# indexing a string - python starts at 0 too (zero-based indexing)
+
+print("Spaghetti"[0])
+print("Spaghetti"[4])
+
+# python allows negative indexes to access a charater from the end of the string
+
+print("hello"[-1])
+print("hello"[-4])
+
+#python also lets you get the range of indexes
+
+print("Spaghetti"[1:4])  # => pag
+print("Spaghetti"[4:-1])    # => hett
+print("Spaghetti"[4:4])  # => (empty string)
+
+#A shortcut for the beginning of the string is to omit the first number.
+
+print("Spaghetti"[:4])  # => Spag
+print("Spaghetti"[:-1])    # => Spaghett
+
+#range does not through errors if it is out of bounds
+
+# USING STRING FUNCTIONS
