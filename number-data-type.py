600ef7527bc211de7a5b56a813cfdfbbb38e1e08
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-30 15:34:55 -0600

    integer notes

--- number-data-type.py
+++ number-data-type.py
@@ -1,5 +1,9 @@
 #Python distinguishes between 3 number types
 
+#integer - counting numbers, positive and negitive. NO DECIMAL POINT
+
+#Integers are created by a literal number with no decimal point or through the use of the int() constructor
+
 print(3)
 print(int(19))
 print(int())
