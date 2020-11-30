7626b1c0050d64ad0a98f1e4645af9d33cd75d78
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-30 16:03:32 -0600

    calc strng length

---
+++ string-data-type.py
@@ -0,0 +1,14 @@
+# you can wrap strings with single and double quotes
+
+# multiline strings - python allows strings to span multiple lines using what's called triple quotes
+
+print('''My instructions are very long so to make them
+more readable in the code I am putting them on
+more than one line. I can even include "quotes"
+of any kind because they won't get confused with
+the end of the string!''')
+
+# calculating length - check how long a string is using len()
+
+print(len("Spaghetti"))
+
