0bad7250d621a1f99b9da339047bac459f95d6f2
miguelcoria94 <miguelcoria94@gmail.com>
2020-12-01 17:54:27 -0600

    string to int

---
+++ string-to-integer.py
@@ -0,0 +1,8 @@
+# Write your function, here.
+def string_int(string):
+    return int(string)
+
+
+print(string_int("6"))  # > 6
+print(string_int("1000"))  # > 1000
+print(string_int("12"))  # > 12
