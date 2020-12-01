ff7712e291ac651b1fc6beea67077905ff551a47
miguelcoria94 <miguelcoria94@gmail.com>
2020-12-01 17:48:05 -0600

    remainder

---
+++ remainder.py
@@ -0,0 +1,9 @@
+# Write your function, here.
+
+def remainder(num1, num2):
+    return num1 % num2
+
+print(remainder(1, 3))  # > 1
+print(remainder(3, 4))  # > 3
+print(remainder(5, 5))  # > 0
+print(remainder(7, 2))  # > 1
