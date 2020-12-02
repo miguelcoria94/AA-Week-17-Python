4b0e6643c29e7c314b5be6b429e2d9d4b5b02a7d
miguelcoria94 <miguelcoria94@gmail.com>
2020-12-01 18:04:31 -0600

    power

---
+++ power.py
@@ -0,0 +1,8 @@
+# Write your function, here.
+def calculate_exponent(num1, num2):
+    return num1 ** num2
+
+
+print(calculate_exponent(5, 5))  # > 3125
+print(calculate_exponent(10, 10))  # > 10000000000
+print(calculate_exponent(3, 3))  # > 27
