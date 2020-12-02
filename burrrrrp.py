db9279e3d448e64e2402ca68024b09328dfb92c7
miguelcoria94 <miguelcoria94@gmail.com>
2020-12-01 18:12:55 -0600

    burrp

---
+++ burrrrrp.py
@@ -0,0 +1,8 @@
+# Write your function, here.
+def long_burp(num):
+    return 'bu' + (num * 'r') + 'p'
+
+
+print(long_burp(3))  # > "Burrrp"
+print(long_burp(5))  # > "Burrrrrp"
+print(long_burp(9))  # > "Burrrrrrrrrp"
