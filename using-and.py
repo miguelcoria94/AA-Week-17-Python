5fdcd3db0f753876ab14a1427bfa8414a67929e3
miguelcoria94 <miguelcoria94@gmail.com>
2020-12-01 18:08:18 -0600

    and

---
+++ using-and.py
@@ -0,0 +1,10 @@
+# Write your function, here.
+
+def And(a,b):
+    return a and b
+
+
+print(And(True, False))  # > False
+print(And(True, True))  # > True
+print(And(False, True))  # > False
+print(And(False, False))  # > False
