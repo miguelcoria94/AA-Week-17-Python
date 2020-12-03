107fbfa5c4a2d4f13544c26f0e28655f5f824c74
miguelcoria94 <miguelcoria94@gmail.com>
2020-12-02 18:39:49 -0600

    testing ranges

--- range.py
+++ range.py
@@ -3,15 +3,20 @@ print('A) End Value')
 
 # STEP 1: Change the zero in the range to 10
 #         Notice how "10" is not included in the output
-for i in range(0):
+for i in range(10):
     print(i)
 
 print('B) Start & End Values')
 
 # STEP 2: Code a `for` loop to print numbers 5 through 9
+for i in range(5,10):
+    print(i)
 
 
 print('C) Only Even Values')
 
 # STEP 3: Print 0, 2, 4, 6 and 8 using a for loop
 #         Hint - range can take a 3rd parameter for the step distance
+
+for i in range(0,9,2):
+    print(i)
