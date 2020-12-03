e319e9c1f3a91f9661715503a9932aad5fd80bce
miguelcoria94 <miguelcoria94@gmail.com>
2020-12-02 18:28:29 -0600

    tuples

--- explore-tuple.py
+++ explore-tuple.py
@@ -7,18 +7,18 @@ evens = 2, 4, 6, 8
 
 
 # Step 2: Print out the result of multiplying odds by three
-print( odds * 3)
+#print( odds * 3)
 
 
 # Step 3A: Use print to find out if odds is less than evens
-print()
+#print(odds < evens)
 
 
 # Step 3B: Print out your explanation of why 3A has that result
-
+#goes by the first num in each tuple
 
 # Step 4: Print out the average of the numbers in evens using one line of code
-
+print(sum(evens) / len(evens))
 
 # Step 5A: Write a function 'minmaxmean' that accepts an iterable and
 #         returns the minimum value, the maximum value and the average (mean)
