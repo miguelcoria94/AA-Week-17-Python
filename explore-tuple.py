7c0ebb80e6d91d1458604bf9b8c64d547386d4fc
miguelcoria94 <miguelcoria94@gmail.com>
2020-12-02 18:20:30 -0600

    switch

---
+++ explore-tuple.py
@@ -0,0 +1,28 @@
+# DO NOT EDIT
+odds = 1, 3, 5, 7, 9
+evens = 2, 4, 6, 8
+
+# Step 1: Print out the result of adding evens to odds
+
+
+# Step 2: Print out the result of multiplying odds by three
+
+
+# Step 3A: Use print to find out if odds is less than evens
+
+
+# Step 3B: Print out your explanation of why 3A has that result
+
+
+# Step 4: Print out the average of the numbers in evens using one line of code
+
+
+# Step 5A: Write a function 'minmaxmean' that accepts an iterable and
+#         returns the minimum value, the maximum value and the average (mean)
+
+
+# Step 5B: Use print to confirm you function is working on evens and odds
+
+
+# BONUS: Call your function with a new tuple of your own creation
+#        And print the results in a pretty way
