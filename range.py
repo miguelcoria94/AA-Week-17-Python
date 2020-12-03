86cd3f73256d1119907d21d22344db5a18aecf03
miguelcoria94 <miguelcoria94@gmail.com>
2020-12-02 18:55:21 -0600

    test function

--- range.py
+++ range.py
@@ -34,3 +34,27 @@ def get_sum_of_elements(list):
 print(get_sum_of_elements([2, 7, 4]))  # > 13
 print(get_sum_of_elements([45, 3, 0]))  # > 48
 print(get_sum_of_elements([-2, 84, 23]))  # > 105
+
+
+# Write your function, here.
+
+def get_indices(list, val):
+    indicies = []
+    for i in range(0, len(list)):
+        if val == list[i]:
+            indicies.append(i)
+    return indicies
+    
+        
+
+print(get_indices(["a", "a", "b", "a", "b", "a"], "a"))
+# Prints [0, 1, 3, 5]
+
+print(get_indices([1, 5, 5, 2, 7], 7))
+# Prints [4]
+
+print(get_indices([1, 5, 5, 2, 7], 5))
+# Prints [1, 2]
+
+print(get_indices([1, 5, 5, 2, 7], 8))
+# Prints []
